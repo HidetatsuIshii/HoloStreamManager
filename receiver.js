414996b851d1b4e8d6d51bff5cc67f094a35f3ff
Hidetatsu Ishii <ishii@toho-next.com>
2026-04-08 18:14:18 +0900

    Update receiver.js

--- receiver.js
+++ receiver.js
@@ -1,4 +1,14 @@
+// 【受信】Backgroundから届いたコメントを、index.htmlに渡す
 chrome.runtime.onMessage.addListener((request) => {
-    // GitHub Pages上の元のJS(index.html)にデータを渡す
     window.postMessage({ type: "FROM_EXTENSION", data: request }, "*");
+});
+
+// 【送信】index.htmlから届いた「取得開始」命令を、Backgroundに渡す
+window.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "START_POLLING_REQUEST") {
+        chrome.runtime.sendMessage({ 
+            type: "START_POLLING", 
+            videoIds: event.data.videoIds 
+        });
+    }
 });
